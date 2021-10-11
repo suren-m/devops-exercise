a463649875b836d1f451a5fab4e8b4e962974b9e
suren <suren2535@hotmail.com>
2021-10-11 09:40:00 +0100

    feat: aks cluster

---
+++ 2_k8s/aks_cluster_provisioning/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "azurerm" {
+  
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.80.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks-rg" {
+  name     = "devops-exercise"
+  location = "UK South"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devops-exercise"
+  location            = azurerm_resource_group.aks-rg.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = "devops-exercise"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2as_v4"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "demo"
+  }
+}

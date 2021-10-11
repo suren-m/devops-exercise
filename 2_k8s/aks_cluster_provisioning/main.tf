58f032163fa72acfdb68c696b437a61458861b4a
suren <suren2535@hotmail.com>
2021-10-11 09:56:47 +0100

    feat: aks cluster

--- 2_k8s/aks_cluster_provisioning/main.tf
+++ 2_k8s/aks_cluster_provisioning/main.tf
@@ -24,11 +24,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.aks-rg.location
   resource_group_name = azurerm_resource_group.aks-rg.name
   dns_prefix          = "devops-exercise"
+  kubernetes_version  = "1.21.2"
 
   default_node_pool {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2as_v4"
+    orchestrator_version = "1.21.2"    
   }
 
   identity {
